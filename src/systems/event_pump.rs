447d7be193ec1f69f9c52dac8afda475bba23b49
Henrique Navarro <ricksnavarro@gmail.com>
2021-05-12 23:07:54 -0400

    Add input

--- src/systems/event_pump.rs
+++ src/systems/event_pump.rs
@@ -3,14 +3,18 @@ use sdl2::keyboard::Keycode;
 
 pub struct EventPump {
     event_pump: sdl2::EventPump,
-    pub close_game: bool
+    pub close_game: bool, 
+    pub go_left: bool,
+    pub go_right: bool
 }
 
 impl EventPump {
     pub fn new(event_pump: sdl2::EventPump) -> Self {
         Self {
             event_pump,
-            close_game: false
+            close_game: false,
+            go_left: false,
+            go_right: false
         }        
     }
 
@@ -18,7 +22,21 @@ impl EventPump {
         for event in self.event_pump.poll_iter() {
             match event {
                 Event::KeyDown { keycode: Some(Keycode::Escape), .. } 
-                | Event::Quit { .. } => { self.close_game = true; },
+                | Event::Quit { .. } => { 
+                    self.close_game = true; 
+                },
+                Event::KeyDown { keycode: Some(Keycode::A), .. } => {
+                    self.go_left = true; 
+                }, 
+                Event::KeyUp { keycode: Some(Keycode::A), .. } => {
+                    self.go_left = false; 
+                }, 
+                Event::KeyDown { keycode: Some(Keycode::D), .. } => {
+                    self.go_right = true; 
+                }, 
+                Event::KeyUp { keycode: Some(Keycode::D), .. } => {
+                    self.go_right = false; 
+                }, 
                 _ => {}
            } 
         }
