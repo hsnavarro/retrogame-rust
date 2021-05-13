4db36ae996a6300faa291dc8c9b149af947249ce
Henrique Navarro <ricksnavarro@gmail.com>
2021-05-12 23:13:12 -0400

    Add FPS, player and ball constants

--- src/game_settings.rs
+++ src/game_settings.rs
@@ -1,2 +1,12 @@
-pub const SCREEN_HEIGHT: u32 = 600;
-pub const SCREEN_WIDTH: u32 = 800;
+pub const SCREEN_WIDTH: u32 = 960;
+pub const SCREEN_HEIGHT: u32 = 540;
+
+pub const FIXED_FPS: u32 = 120;
+pub const FIXED_DELTA_TIME: f64 = 1.0 / FIXED_FPS as f64;
+
+pub const PLAYER_VELOCITY: f64 = 300.0; 
+pub const PLAYER_WIDTH: f64 = 200.0;
+pub const PLAYER_HEIGHT: f64 = 20.0;
+
+pub const BALL_VELOCITY: f64 = 300.0;
+pub const BALL_RADIUS: f64 = 10.0;
